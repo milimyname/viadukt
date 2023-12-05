67f583b956e89668167840e109dca66fdad75898
mili <kamil.maksudov2019@gmail.com>
2023-12-05 23:55:49 +0100

    ci: deploy to fly

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,8 @@
-import { auth } from "@/auth/lucia";
-import * as context from "next/headers";
 import { redirect } from "next/navigation";
+import { getPageSession } from "../layout";
 
 const Page = async () => {
-  const authRequest = auth.handleRequest("GET", context);
-  const session = await authRequest.validate();
+  const session = await getPageSession();
   if (session) redirect("/");
   return (
     <>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import { auth } from "@/auth/lucia";
-import * as context from "next/headers";
 import { redirect } from "next/navigation";
 
 import Form from "@/components/form"; // expect error - see next section
+import { getPageSession } from "./layout";
 
 const Page = async () => {
-  const authRequest = auth.handleRequest("GET", context);
-  const session = await authRequest.validate();
+  const session = await getPageSession();
   if (!session) redirect("/login");
   return (
     <>
       <h1>Profile</h1>
       <p>User id: {session.user.userId}</p>
-      <p>Username: {session.user.username}</p>
+      <p>Username: {session.user.githubUsername}</p>
       <Form action="/api/logout">
         <input type="submit" value="Sign out" />
       </Form>
