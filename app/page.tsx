7ce5b69cda8dbceebb285dc02678c9902998e50c
mili <kamil.maksudov2019@gmail.com>
2023-12-07 21:20:50 +0100

    ui: add react query builder

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,48 @@
 import { redirect } from "next/navigation";
-
-import Form from "@/components/form"; // expect error - see next section
 import { getPageSession } from "@/lib/getSession";
+import { unstable_noStore } from "next/cache";
+import QueryBuilderComponent from "@/components/QueryBuilderComponent";
+import Link from "next/link";
+import { CustomSheet } from "@/components/CustomSheet";
+import { AccountDropdown } from "@/components/AccountDropdown";
 
 const Page = async () => {
+  // unstable_noStore();
   const session = await getPageSession();
   if (!session) redirect("/login");
+
   return (
-    <main className="h-screen flex items-center justify-center">
-      <div className=" max-w-md p-10 gap-5 flex border rounded-md flex-col justify-center items-center ">
-        <h1 className="font-bold text-4xl">Profile</h1>
-        <p>User id: {session.user.userId}</p>
-        <p>Username: {session.user.githubUsername}</p>
-        <Form action="/api/logout">
-          <input type="submit" value="Sign out" />
-        </Form>
-      </div>
-    </main>
+    <>
+      <header className="flex h-20 px-8 py-2 items-center justify-between border-b">
+        <div className="flex gap-5">
+          <CustomSheet />
+          <ul className="flex gap-4 items-center">
+            <li className="text-gray-600 hover:text-gray-600 ">
+              <Link href="/">Überblick</Link>
+            </li>
+            <li className=" text-gray-400 hover:text-gray-600 ">
+              <Link href="/">Produkte</Link>
+            </li>
+            <li className=" text-gray-400 hover:text-gray-600 ">
+              <Link href="/">Einstellungen</Link>
+            </li>
+            <li className=" text-gray-400 hover:text-gray-600 ">
+              <Link href="/">Über Uns</Link>
+            </li>
+          </ul>
+        </div>
+
+        <AccountDropdown
+          session={{
+            username: session.user.username,
+            url: session.user.avatar_url,
+          }}
+        />
+      </header>
+      <main>
+        <QueryBuilderComponent userId={session.user.userId} />
+      </main>
+    </>
   );
 };
 
