752306bd9cc0d0696b3eb46309e5d1cb59a31e0a
mili <kamil.maksudov2019@gmail.com>
2023-12-14 12:35:19 +0100

    fix: add ci/cd n comment out routes

--- app/api/calc/route.ts
+++ app/api/calc/route.ts
@@ -1,97 +1,97 @@
-import type { NextRequest } from "next/server";
-import { calculationSchema } from "@/lib/validationSchema";
-import prisma from "@/prisma/client";
+// import type { NextRequest } from "next/server";
+// import { calculationSchema } from "@/lib/validationSchema";
+// import prisma from "@/prisma/client";
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = calculationSchema.safeParse(body);
+// export async function POST(request: NextRequest) {
+//   const body = await request.json();
+//   const validation = calculationSchema.safeParse(body);
 
-  // Check that the request has the required parameters
-  if (!validation.success) {
-    return Response.json(validation.error.format(), {
-      status: 400,
-    });
-  }
+//   // Check that the request has the required parameters
+//   if (!validation.success) {
+//     return Response.json(validation.error.format(), {
+//       status: 400,
+//     });
+//   }
 
-  try {
-    const calculation = await prisma.calculation.create({
-      data: {
-        user_id: body.user_id,
-        selectedOperator: "+",
-        result: 0,
-        name: body.name,
-        type: body.type,
-        schema: body.schema,
-      },
-    });
+//   try {
+//     const calculation = await prisma.calculation.create({
+//       data: {
+//         user_id: body.user_id,
+//         selectedOperator: "+",
+//         result: 0,
+//         name: body.name,
+//         type: body.type,
+//         schema: body.schema,
+//       },
+//     });
 
-    return Response.json({
-      calculationId: calculation.id,
-    });
-  } catch (c) {
-    return Response.json({ message: c }, { status: 500 });
-  }
-}
+//     return Response.json({
+//       calculationId: calculation.id,
+//     });
+//   } catch (c) {
+//     return Response.json({ message: c }, { status: 500 });
+//   }
+// }
 
-export async function PATCH(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const costBlocks = JSON.parse(body.costBlocks);
+// export async function PATCH(request: NextRequest) {
+//   try {
+//     const body = await request.json();
+//     const costBlocks = JSON.parse(body.costBlocks);
 
-    if (costBlocks[0])
-      await prisma.costBlock.update({
-        where: {
-          id: costBlocks[0].id,
-        },
-        data: {
-          name: costBlocks[0].name,
-          description: costBlocks[0].description,
-          value: costBlocks[0].value,
-        },
-      });
+//     if (costBlocks[0])
+//       await prisma.costBlock.update({
+//         where: {
+//           id: costBlocks[0].id,
+//         },
+//         data: {
+//           name: costBlocks[0].name,
+//           description: costBlocks[0].description,
+//           value: costBlocks[0].value,
+//         },
+//       });
 
-    if (costBlocks[1])
-      await prisma.costBlock.update({
-        where: {
-          id: costBlocks[1].id,
-        },
-        data: {
-          name: costBlocks[1].name,
-          description: costBlocks[1].description,
-          value: costBlocks[1].value,
-        },
-      });
+//     if (costBlocks[1])
+//       await prisma.costBlock.update({
+//         where: {
+//           id: costBlocks[1].id,
+//         },
+//         data: {
+//           name: costBlocks[1].name,
+//           description: costBlocks[1].description,
+//           value: costBlocks[1].value,
+//         },
+//       });
 
-    const calculation = await prisma.calculation.update({
-      where: {
-        id: body.id,
-      },
-      data: {
-        selectedOperator: body.selectedOperator,
-        schema: body.schema,
-      },
-    });
+//     const calculation = await prisma.calculation.update({
+//       where: {
+//         id: body.id,
+//       },
+//       data: {
+//         selectedOperator: body.selectedOperator,
+//         schema: body.schema,
+//       },
+//     });
 
-    return Response.json({
-      calculationId: calculation.id,
-    });
-  } catch (c) {
-    return Response.json({ message: c }, { status: 500 });
-  }
-}
+//     return Response.json({
+//       calculationId: calculation.id,
+//     });
+//   } catch (c) {
+//     return Response.json({ message: c }, { status: 500 });
+//   }
+// }
 
-export async function DELETE(request: NextRequest) {
-  try {
-    const body = await request.json();
+// export async function DELETE(request: NextRequest) {
+//   try {
+//     const body = await request.json();
 
-    await prisma.calculation.delete({
-      where: {
-        id: body.calculationId,
-      },
-    });
+//     await prisma.calculation.delete({
+//       where: {
+//         id: body.calculationId,
+//       },
+//     });
 
-    return Response.json({ message: "success" });
-  } catch (c) {
-    return Response.json({ message: c }, { status: 500 });
-  }
-}
+//     return Response.json({ message: "success" });
+//   } catch (c) {
+//     return Response.json({ message: c }, { status: 500 });
+//   }
+// }
--- app/api/calculation/route.ts
+++ app/api/calculation/route.ts
@@ -1,45 +1,45 @@
-import jsonLogic from "json-logic-js";
-import type { NextRequest } from "next/server";
-// import { calculationSchema } from "@/lib/validationSchema";
-// import prisma from "@/prisma/client";
-// import { logic } from "@/lib/utils";
+// import jsonLogic from "json-logic-js";
+// import type { NextRequest } from "next/server";
+// // import { calculationSchema } from "@/lib/validationSchema";
+// // import prisma from "@/prisma/client";
+// // import { logic } from "@/lib/utils";
 
-export async function GET(request: NextRequest) {
-  return Response.json({ message: "Hello from Next.js!" });
-}
+// export async function GET(request: NextRequest) {
+//   return Response.json({ message: "Hello from Next.js!" });
+// }
 
-interface DataObject {
-  [key: string]: string;
-}
+// interface DataObject {
+//   [key: string]: string;
+// }
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
+// export async function POST(request: NextRequest) {
+//   const body = await request.json();
 
-  // Convert body data to an object
+//   // Convert body data to an object
 
-  const data = body.data.reduce(
-    (acc: DataObject, cur: { name: string; value: string }) => {
-      acc[cur.name] = cur.value;
-      return acc;
-    },
-    {} as DataObject
-  );
+//   const data = body.data.reduce(
+//     (acc: DataObject, cur: { name: string; value: string }) => {
+//       acc[cur.name] = cur.value;
+//       return acc;
+//     },
+//     {} as DataObject
+//   );
 
-  try {
-    // Calculate the energy savings by using the json-logic-js library
-    //@ts-ignore
-    const result = jsonLogic.apply(
-      {
-        "+": [{ var: "a" }, { var: "b" }],
-      },
-      data
-    );
+//   try {
+//     // Calculate the energy savings by using the json-logic-js library
+//     //@ts-ignore
+//     const result = jsonLogic.apply(
+//       {
+//         "+": [{ var: "a" }, { var: "b" }],
+//       },
+//       data
+//     );
 
-    return Response.json({
-      type: "JsonLogic: Addition",
-      result,
-    });
-  } catch (c) {
-    return Response.json({ message: c }, { status: 500 });
-  }
-}
+//     return Response.json({
+//       type: "JsonLogic: Addition",
+//       result,
+//     });
+//   } catch (c) {
+//     return Response.json({ message: c }, { status: 500 });
+//   }
+// }
