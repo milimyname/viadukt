4b119e99941cdc2a8bf1f3c1ff6c94a9a675d56b
mili <kamil.maksudov2019@gmail.com>
2023-12-14 12:39:16 +0100

    fix: comment out calculation page for now

--- app/calculation/[slug]/page.tsx
+++ app/calculation/[slug]/page.tsx
@@ -1,33 +1,33 @@
-import { getPageSession } from "@/lib/getSession";
-import { redirect } from "next/navigation";
-import Navbar from "@/components/Navbar";
-import Calculator from "@/components/Calculator";
-import prisma from "@/prisma/client";
+// import { getPageSession } from "@/lib/getSession";
+// import { redirect } from "next/navigation";
+// import Navbar from "@/components/Navbar";
+// import Calculator from "@/components/Calculator";
+// import prisma from "@/prisma/client";
 
-const Page = async ({ params }: { params: { slug: string } }) => {
-  const session = await getPageSession();
-  if (!session) redirect("/login");
+// const Page = async ({ params }: { params: { slug: string } }) => {
+//   const session = await getPageSession();
+//   if (!session) redirect("/login");
 
-  // Get the current calculation
-  const calculation = await prisma.calculation.findUnique({
-    where: { id: +params.slug },
-    include: { costBlocks: true },
-  });
+//   // Get the current calculation
+//   const calculation = await prisma.calculation.findUnique({
+//     where: { id: +params.slug },
+//     include: { costBlocks: true },
+//   });
 
-  if (!calculation) redirect("/");
+//   if (!calculation) redirect("/");
 
-  return (
-    <>
-      <Navbar
-        username={session.user.username}
-        avatar_url={session.user.avatar_url}
-        userId={session.user.userId}
-      />
-      <main className="p-10 flex w-full justify-center gap-20 sm:px-0">
-        <Calculator calculation={calculation} />
-      </main>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Navbar
+//         username={session.user.username}
+//         avatar_url={session.user.avatar_url}
+//         userId={session.user.userId}
+//       />
+//       <main className="p-10 flex w-full justify-center gap-20 sm:px-0">
+//         <Calculator calculation={calculation} />
+//       </main>
+//     </>
+//   );
+// };
 
-export default Page;
+// export default Page;
